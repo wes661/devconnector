197f7be58c707733ef7aa3a47749870015e459f7
Wesley Hanson <wrh1600@gmail.com>
2018-06-13 23:26:47 -0700

    user register/login and auth with web tokens

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://wes:wesley16@ds159100.mlab.com:59100/devconnector"
+  mongoURI: "mongodb://wes:wesley16@ds159100.mlab.com:59100/devconnector",
+  secretOrKey: "secret"
 };
