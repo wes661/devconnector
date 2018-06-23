50811fd81aa56c3b18043e273efe875e792911be
Wesley Hanson <wrh1600@gmail.com>
2018-06-22 23:12:30 -0700

    progress

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,11 +8,62 @@ const validatePostInput = require("../../validation/post");
 
 //Load post model
 const Post = require("../../models/Post");
+// Profile model
+const Profile = require("../../models/Profile");
 
 //@route  GET api/posts/test
 //@desc   Test post route
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "posts works" }));
+
+//@route  GET api/posts
+//@desc   Get posts
+//@access Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopost: "No posts found" }));
+});
+
+//@route  GET api/posts/:id
+//@desc   Get posts by id
+//@access Public
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopost: "No post found with that ID" })
+    );
+});
+
+//@route  DELETE api/posts/:id
+//@desc   Delete post
+//@access Private
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          //Delete
+          post.remove().then(() => res.json({ seccess: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+    });
+  }
+);
 
 //@route  POST api/posts
 //@desc   Create post
@@ -22,14 +73,22 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      //If errors send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
 
-    newPost.save().then()(post => res.json(post));
+    newPost.save().then(post => res.json(post));
   }
 );
 
